dab59abc0d38d5c4a870e6c6a418d853012626c6
Andy Chan <andy.chan9930@gmail.com>
2022-05-13 14:24:27 -0400

    commit tree b496323eab5cd4fae2656361aeca30a22e051733

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def show
+        user = User.find_by(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def render_not_found_response
+      render json: { error: "Dog house not found" }, status: :not_found
+    end
 end
